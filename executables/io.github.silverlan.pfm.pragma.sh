b64fba2250fa09de617d55c075871ac816445907
Silverlan <silverlan@pragma-engine.com>
2025-07-21 17:53:01 +0200

    feat: add flatpak configuration files

---
+++ executables/io.github.silverlan.pfm.pragma.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+exec "/app/pragma" -user_data_dir "~/.config/pragma" "$@"
---
+++ executables/io.github.silverlan.pfm.pragma_server.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+exec "/app/pragma_server" -user_data_dir "~/.config/pragma_server" "$@"
