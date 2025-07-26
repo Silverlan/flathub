2aa86cdbd06c0c7965288126e022d0cf91991bb3
Silverlan <silverlan@pragma-engine.com>
2025-07-26 16:57:45 +0200

    feat: now launched with -managed_by_package_manager
    For disabling automatic updates by the program itself.

--- executables/io.github.silverlan.pfm.sh
+++ executables/io.github.silverlan.pfm.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-exec "/app/pfm" -user_data_dir "~/.var/app/io.github.silverlan.pfm/data" "$@"
+exec "/app/pfm" -managed_by_package_manager -user_data_dir "~/.var/app/io.github.silverlan.pfm/data" "$@"
