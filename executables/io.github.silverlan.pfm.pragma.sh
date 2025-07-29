115d198bc2e91ab21e32ff6357a74f2a2aca217b
Silverlan <silverlan@pragma-engine.com>
2025-07-29 16:47:48 +0200

    feat: add "-sandboxed" launch option

--- executables/io.github.silverlan.pfm.pragma.sh
+++ executables/io.github.silverlan.pfm.pragma.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-exec "/app/pragma" -managed_by_package_manager -user_data_dir "~/.var/app/io.github.silverlan.pfm/data" "$@"
+exec "/app/pragma" -managed_by_package_manager -sandboxed -user_data_dir "~/.var/app/io.github.silverlan.pfm/data" "$@"
